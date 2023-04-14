55156910a0588e2de1e0a63f5d8c16c026897d03
Divyansh Singh <singh.divyansh1802@gmail.com>
2023-04-14 12:53:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-// complete this js code
-function Person(name, age) {}
+// Person constructor function
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+// Person prototype method
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+}
+
+// Employee constructor function
+function Employee(name, age, jobTitle) {
+  // call the Person constructor function and set its properties
+  Person.call(this, name, age);
+
+  // set the Employee-specific property
+  this.jobTitle = jobTitle;
+}
+
+// Inherit the Person prototype
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+// Employee prototype method
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+}
 
 // Do not change code below this line
 window.Person = Person;
